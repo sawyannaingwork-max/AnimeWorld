ef81f4e26471f828ab4527e03e75cf8468e1b2eb
sawyannaingwork-max <sawyannaingwork@gmail.com>
2026-02-12 11:23:37 +0700

    Adding hero section of home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import Header from "./components/Header"
+import { Routes, Route } from 'react-router-dom'
+import Home from "./home/Home"
 
 export default function App()
 {
-  return <Header />
+    return(
+        <div className="bg-background">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    )
 }
